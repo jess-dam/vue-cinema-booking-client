2c0898b369a16bf1c95ffa27a214fd3cc5d0f89d
JDXue <jessica.dam@yahoo.com>
2022-06-10 10:12:58 +0100

    Add basic booking form

---
+++ Dev/vue-table-booking-client/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <BookingForm />    
+  </div>
+</template>
+
+<script>
+import BookingForm from './components/BookingForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BookingForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+}
+
+h1,h2,h3,h4,label {
+  color: #7eebf3;
+}
+
+input {
+  padding: 20px;
+  border-radius: 10px;
+  margin: 20px;
+  min-width: 300px;
+  width: 70%;
+}
+
+button {
+  background: #7eebf3;
+  padding: 10px;
+  border-radius: 40px;
+  border-color: #7eebf3;
+  min-width: 300px;
+  margin: 30px;
+}
+
+form {
+    padding: 20px;
+    background: #333333;
+    border-radius: 10px;
+    min-width: 400px;
+    width: 70%;
+    height: auto;
+    display: grid;
+    justify-items: center;
+    margin: 30px;
+}
+
+textarea {
+    min-width: 300px;
+    width: 70%;
+    margin: 30px;
+}
+</style>
---
+++ Dev/vue-table-booking-client/src/components/BookingForm.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h1>Book Cinema Tickets</h1>
+        <form @submit='submit'>
+            <label>Number of Tickets</label>
+            <select v-model='numberOfTickets'>
+                   <option v-for='option in numberOfTicketsOptions' :value='option.value'>
+                        {{ option.text }}
+                    </option>
+            </select>
+                        
+            <label>Date</label>
+            <input placeholder='Date' v-model='date'/>
+
+            <label>Screening</label>
+            <input placeholder='Time' v-model='screeningTime'/>
+
+            <label>Notes</label>
+            <textarea placeholder='Add any special requests here' v-model='notes'/>
+
+            <button type='submit'>Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    // const ticketBatchLimit = 10
+    // const ticketBatchOptions = () => {
+    //     let options = []
+    //     for ( let x=1; x <= ticketBatchLimit; x++) {
+    //         options.push({ text: `${x}`, value: x })
+    //     }
+    //     return options
+    // }
+
+    name: 'bookingForm',
+    data() {
+        return {
+            ticketBatchLimit: 10,
+            numberOfTicketsOptions:[],
+            screenTimes: [ '10:00', '10:30', '12:00', '13:00', '14:00', '18:00'],
+            numberOfTickets: '1',
+            date: '',
+            screeningTime: '',
+            notes: ''
+        }
+    },
+    methods: {
+        submit () {
+            const { numberOfTickets, date, screeningTime, notes } = this
+            localStorage.setItem('ticketRequest', JSON.stringify({
+                numberOfTickets,
+                date,
+                screeningTime,
+                notes 
+            }))
+        } 
+    }
+}
+</script>
+
