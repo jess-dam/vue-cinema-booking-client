66d77d1057162bf9b8c740df526ae7719be78984
JDXue <jessica.dam@yahoo.com>
2022-06-10 22:49:21 +0100

    initial commit

---
+++ src/components/BookingForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <h1>Book Cinema Tickets</h1>
+        <form @submit='submit'>
+            <label>Number of Tickets</label>
+            <select v-model='numberOfTickets'>
+                   <option v-for='option in numberOfTicketsOptions' :value='option.value'>
+                        {{ option.text }}
+                    </option>
+            </select>
+                        
+            <label>Date</label>
+            <input placeholder='Date' v-model='date'/>
+
+            <label>Screening</label>
+            <select v-model='screeningTime'>
+                   <option v-for='option in screenTimesOptions' :value='option.value'>
+                        {{ option.text }}
+                    </option>
+            </select>
+
+            <label>Notes</label>
+            <textarea placeholder='Add any special requests here' v-model='notes'/>
+
+            <button type='submit'>Submit</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const numberOfTickets = ref(1)
+const date = ref('')
+const screeningTime = ref('')
+const notes = ref('')
+
+const ticketBatches = ['1', '2', '3', '4', '5']
+const numberOfTicketsOptions = getSelectOptionsForList(ticketBatches)
+const screenTimes = [ '10:00', '10:30', '12:00', '13:00', '14:00', '18:00']
+const screenTimesOptions = getSelectOptionsForList(screenTimes)
+
+function getSelectOptionsForList (options) {
+    return options.map( o => ({ text: o.toString(), value: o }))
+}
+
+function submit() {
+    ('Form submitted!')    
+    // localStorage.setItem('ticketRequest', JSON.stringify({
+    //     numberOfTickets,
+    //     date,
+    //     screeningTime,
+    //     notes 
+    // }))
+} 
+
+onMounted(() => {
+    console.log(date)
+})
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// import { Button, Select } from 'element-ui';
+
+// Vue.component(Button.name, Button);
+// Vue.component(Select.name, Select);
+/* or
+ * Vue.use(Button)
+ * Vue.use(Select)
+ */
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// });
+
+createApp(App).mount('#app')
